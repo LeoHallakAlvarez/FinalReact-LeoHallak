c7164974869969c3058ba86b3db3178ab9326e86
LeoHallakAlvarez <detroitborg92@hotmail.com>
2023-10-04 20:07:04 -0300

    acceso

---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { CartContext } from "../../context/CartContext.jsx";
+import CartItem from "../CartItem/";
+import { Link } from "react-router-dom";
+import styles from "./cart.module.css";
+import "./cart.css";
+
+const Cart = () => {
+  const { cart, clear, getTotal } = useContext(CartContext);
+
+  return (
+    <div className={styles.carrito}>
+      <h1>Carrito</h1>
+      {cart.length !== 0 ? (
+        <div className="container">
+          <div className="row">
+            {cart.map((e) => (
+              <CartItem key={e.id} product={e} />
+            ))}
+            <div>{`Total Compra:  $ ${getTotal()}`}</div>
+            <button className="botonRojo btn btn-danger" onClick={clear}>
+              Vaciar Carrito
+            </button>
+            <Link to="/checkout">
+              <button className="botonVerde btn btn-success">
+                Finalizar Compra
+              </button>
+            </Link>
+          </div>
+        </div>
